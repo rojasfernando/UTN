84d3799c00a95c45f4af561443707cde644f56c8
Fernando Rojas <rojasfj@gmail.com>
2023-10-24 22:30:17 -0600

    python I

---
+++ PycharmProjects/pythonProject/TecnicaturaPython.py
@@ -0,0 +1,44 @@
+a = True
+print (type(a))
+# Tipos int, float, String, Bool
+
+x = 10
+print (x)
+print (type(x))
+x = 14.5
+print (x)
+print (type(x))
+x = "Hola Alumnos"
+print (x)
+x = True
+x = False
+print (x)
+print (type(x))
+
+operandoA = 8
+operandoB = 5
+suma = operandoA + operandoB
+print ()
+
+operandoA - operandoB
+print(f"El resultado de la resta es: {resta}")
+
+multiplicacion |
+
+alto= int(input("Proporcione el alto del rectángulo: "))
+ancho= int(input("Proporcione el ancho del rectángulo: "))
+area= alto*ancho
+perimetro= (alto + ancho)*2
+print ("area: ", area)
+print ("perimetro:", perimetro)
+
+
+num = int(input("Proporcione un número: "))
+valor = int(input("Ingrese el valor: "))
+if valor%2 ==0: print('El valor es par')
+else: print('el valor es impar')
+
+edad= int(input("Digite su edad: "))
+if edad >= 21: print("Es mayor de edad")
+else: print("Es menor de edad")
+
---
+++ PycharmProjects/pythonProject/pureba.py
@@ -0,0 +1,26 @@
+# Manejo de cadenas(String)
+miGrupoFavorito = "Soda Stereo"
+caracteristicas= "Los Redonditos de Ricota"
+print("Mi grupo favorito es: ", miGrupoFavorito, caracteristicas)
+
+numero1 = "7"
+numero2 = "8"
+
+print (int(numero1) + (numero2))
+
+#Tipos booleanos (bool)
+miBooleano = 3<2
+print (miBooleano)
+
+if miBooleano:
+    print("El resultado es verdadero")
+else:
+    print ("<el resultado es falso")
+
+#Procesar la entrada del usuairio4
+#Funsion IMput
+resultado= input ("Digite un numero: ") #regresa un datp tipo sringt
+print (resultado)
+#<conversion en la entrada de datod
+
+
